661eeb4ac65b0454613fd539eb6246c53557a2df
Vismmax <vismmax@gmail.com>
2020-05-10 12:38:53 +0300

    Add createFighterPreview (closes #3 )

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,7 +7,20 @@ export function createFighterPreview(fighter, position) {
     className: `fighter-preview___root ${positionClassName}`,
   });
 
-  // todo: show fighter info (image, name, health, etc.)
+  if (fighter) {
+    const image = createFighterImage(fighter);
+    const name = createElement({tagName:'h2'});
+    name.innerText = fighter.name;
+    const health = createElement({tagName:'div'});
+    health.innerText = `Health: ${fighter.health}`;
+    const attack = createElement({tagName:'div'});
+    attack.innerText = `Attack: ${fighter.attack}`;
+    const defense = createElement({tagName:'div'});
+    defense.innerText = `Defense: ${fighter.defense}`;
+    fighterElement.append(image, name, health, attack, defense);
+  } else if (fighter === false) {
+    fighterElement.innerText = 'Failed to load fighter';
+  }
 
   return fighterElement;
 }
