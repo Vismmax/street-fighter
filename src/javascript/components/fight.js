72a28fbab8e0010fe50ade7b3235042dfda5c051
Vismmax <vismmax@gmail.com>
2020-05-11 07:16:40 +0300

    Add listener keys (closes #24 )

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -3,6 +3,64 @@ import { controls } from '../../constants/controls';
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
     // resolve the promise with the winner when fight is over
+    let fighterOne = {
+      fighter: firstFighter,
+      health: firstFighter.health,
+      blockCriticalHit: false,
+      indicator: document.getElementById('left-fighter-indicator')
+    }
+    let fighterTwo = {
+      fighter: secondFighter,
+      health: firstFighter.health,
+      blockCriticalHit: false,
+      indicator: document.getElementById('right-fighter-indicator')
+    }
+    let pressedKeys = new Set();
+
+    document.addEventListener('keydown', (event) => {
+      pressedKeys.add(event.code);
+      switch (event.code) {
+        case controls.PlayerOneAttack:
+          if (!pressedKeys.has(controls.PlayerOneBlock) && !pressedKeys.has(controls.PlayerTwoBlock)) {
+            strike(fighterOne, fighterTwo);
+          }
+          break;
+        case controls.PlayerTwoAttack:
+          if (!pressedKeys.has(controls.PlayerTwoBlock) && !pressedKeys.has(controls.PlayerOneBlock)) {
+            strike(fighterTwo, fighterOne);
+          }
+          break;
+      }
+      if (!fighterOne.blockCriticalHit &&
+          controls.PlayerOneCriticalHitCombination.includes(event.code) &&
+          checkKeysCriticalHit(controls.PlayerOneCriticalHitCombination, pressedKeys)) {
+        strike(fighterOne, fighterTwo, true);
+      }
+      if (!fighterTwo.blockCriticalHit &&
+          controls.PlayerTwoCriticalHitCombination.includes(event.code) &&
+          checkKeysCriticalHit(controls.PlayerTwoCriticalHitCombination, pressedKeys)) {
+        strike(fighterTwo, fighterOne, true)
+      }
+
+    });
+
+    document.addEventListener('keyup', (event) => {
+      pressedKeys.delete(event.code);
+    });
+
+    function strike(attacker, defender, isCritical=false) {
+      let damage;
+      if (isCritical) {
+        damage = getCriticalDamage(attacker.fighter);
+        attacker.blockCriticalHit = true;
+        setTimeout(() => { attacker.blockCriticalHit = false }, 10000);
+      } else {
+        damage = getDamage(attacker.fighter, defender.fighter);
+      }
+      defender.health -= damage;
+      console.log(defender.health);
+      if (defender.health <= 0) resolve(attacker.fighter);
+    }
   });
 }
 
@@ -29,4 +87,13 @@ export function getBlockPower(fighter) {
   const dodgeChance = Math.random() + 1;
   const power = fighter.defense * dodgeChance;
   return power;
+}
+
+function checkKeysCriticalHit(keys, pressed) {
+  for (let key of keys) {
+    if (!pressed.has(key)) {
+      return false;
+    }
+  }
+  return true;
 }
