a9700a1c2fdf0f0d1a8223ab51a251d1b7e171d0
Vismmax <vismmax@gmail.com>
2020-05-11 09:01:50 +0300

    Add function showWinnerModal (closes #23 )

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,23 @@
+import { showModal } from './modal';
+import { createElement } from '../../helpers/domHelper';
+import { createFighterImage } from '../fighterPreview';
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  const title = `Winner ${fighter.name}`;
+  const bodyElement = createFighter(fighter);
+  showModal({
+    title,
+    bodyElement
+  });
+}
+
+function createFighter(fighter) {
+  const imgElement = createFighterImage(fighter);
+  const fighterElement = createElement({
+    tagName: 'div',
+    className: 'arena___fighter',
+  });
+
+  fighterElement.append(imgElement);
+  return fighterElement;
 }
